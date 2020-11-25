1e9c986e2755f6bbd1185ec8abee82ac96261cea
vandyyu <vandyyu@zhaoxin.com>
2020-11-25 10:25:51 +0800

    init

---
+++ add_default_etc.sh
@@ -0,0 +1,10 @@
+echo "set nu" >> /etc/vim/vimrc
+echo "set hls" >> /etc/vim/vimrc
+echo "set smarttab" >> /etc/vim/vimrc
+echo "set tabstop=4" >> /etc/vim/vimrc
+echo "set shiftwidth=4" >> /etc/vim/vimrc
+echo "set expandtab" >> /etc/vim/vimrc
+echo "set smartindent" >> /etc/vim/vimrc
+echo "\"colorscheme industry" >> /etc/vim/vimrc
+echo "set fileformat=unix" >> /etc/vim/vimrc
+
